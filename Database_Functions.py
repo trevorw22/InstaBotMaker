b79be2221fa0fb1ab6ae7e6f5dc283d6153332a0
mymymy23 <caleb.sanchez@ttu.edu>
2018-04-17 21:16:59 -0500

    Main and Data
    Create a main and data function mostly consisting of pseudocode

---
+++ Database_Functions.py
@@ -0,0 +1,31 @@
+#import openpyxl
+import os
+
+def check_excelsheet():
+    os.chdir('./Data/')
+    if os.path.isfile('profiledata.xlsx'):
+        print ("File Exists");
+    else
+    print ("File Doesn't exist");
+    #Check for 'profiledata.xlsx'
+    #Create an excel sheet if it document doesn't exist
+'''
+def grab_data(CC):
+    wb = openpyx1.load_workbook('profiledata.xlsx')
+    i = 1;
+    #if command code is 0 Find e-mail with null password field and output e-mail
+    if (CC == 0):
+        while true:
+            cell.get_row_letter
+            
+    #if command code is 1 find email- with null IU field and output password
+    if (CC == 1):
+        printf
+
+def input_data(str1, str2, int):
+    wb = openpyx1.load_workbook('profiledata.xlsx')
+    #if str2 is null than input str1 into farthest part of database
+    #if str2 is not null than look at int for which to execute
+    #if command code is 0 input str2 into password field
+    #if command code is 1 input str2 into IU field
+'''
---
+++ generatormain.py
@@ -0,0 +1,15 @@
+from Database_Functions import *
+#main
+
+#Put here how many accounts you want to generate
+Accounts = 5;
+
+
+''' Database '''
+#Scrap for (Accounts) profile pictures
+#Scrap for (5 * Accounts) Memes and alter account
+check_excelsheet();
+
+
+
+
